c7e02be975c8a819230729acbb15f17faf4c7f37
Pooja <pooja.b.d@capgemini.com>
2026-09-25 14:27:12 +0530

    Add logging support

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,19 @@
 package config
 
+import "os"
+
 type Config struct {
 	SendGridAPIKey string
 	TwilioSID      string
 	TwilioToken    string
 	GRPCToken      string
+}
+
+func Load() Config {
+	return Config{
+		SendGridAPIKey: os.Getenv("SENDGRID_API_KEY"),
+		TwilioSID:      os.Getenv("TWILIO_SID"),
+		TwilioToken:    os.Getenv("TWILIO_TOKEN"),
+		GRPCToken:      os.Getenv("GRPC_TOKEN"),
+	}
 }
